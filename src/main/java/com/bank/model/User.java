f0f95dccae5a835830e89b813735b3addda27f7f
ADARSH TYAGI <148191234+Adarsh-tyagi27@users.noreply.github.com>
2026-01-04 19:40:56 +0530

    Add User.java and related setup

---
+++ src/main/java/com/bank/model/User.java
@@ -0,0 +1,37 @@
+package com.bank.model;
+
+public class User {
+    private int id;
+    private String username;
+    private String password; // In a real app, hash this!
+    private String fullName;
+
+    public User(int id, String username, String password, String fullName) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.fullName = fullName;
+    }
+
+    public User(String username, String password, String fullName) {
+        this.username = username;
+        this.password = password;
+        this.fullName = fullName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+}
